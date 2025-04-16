e05e0778f5b7a1b67ba3e859a0d85e74c42a3c40
infowill <152681064+infowill@users.noreply.github.com>
2025-04-16 14:21:30 +0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.py
@@ -0,0 +1,33 @@
+// On start, set ultrasonic pins to 
+// (Trig:P2 | Echo:P12) and
+// set servo position to 0 degree.
+ultrasonic.setUltrasonicTrigEcho(RekabitUltrasonicIOPins.set2)
+rekabit.setServoPosition(ServoChannel.S1, 0)
+// Always check ultrasonic reading. If an object is detected at less than 10 cm away, then...
+// 
+// play 'power up' melody 
+// 
+// wait 500 ms
+// 
+// set servo position to 110 degrees (i.e. to open the mouth and raise the holder to drop the coin.
+// 
+// wait 500 ms
+// 
+// set servo position back to 0 degree (i.e. to close the mouth and return the holder to initial position)
+// 
+// 
+// 
+// 
+// else,
+// 
+// clear screen
+basic.forever(function () {
+    if (ultrasonic.compareDistance(AnalogCompareType.LessThan, 10)) {
+        music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+        basic.pause(500)
+        rekabit.setServoPosition(ServoChannel.S1, 110)
+        basic.pause(500)
+        rekabit.setServoPosition(ServoChannel.S1, 0)
+        basic.pause(1000)
+    }
+})
